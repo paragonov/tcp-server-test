7cfc94b942ed3275177c9c1e46b0796f3d9bea60
Vladimir <vladimirhoris@gmail.com>
2022-09-03 12:43:47 +0700

    add input data

--- telnet-client.py
+++ telnet-client.py
@@ -5,7 +5,7 @@ import openpyxl
 from pathlib import Path
 
 HOST = '127.0.0.1'
-PORT = 8888
+PORT = 8882
 
 
 def processing_file(fn):
@@ -25,7 +25,7 @@ def processing_file(fn):
         wb = openpyxl.open(fn, read_only=True)
         sheet = wb.active
         list_data = []
-        for row in range(2, sheet.max_row+1):
+        for row in range(2, sheet.max_row + 1):
             list_data.append(
                 sheet[row][0].value + ' ' + sheet[row][1].value + ' ' + sheet[row][2].value + ' ' + sheet[row][3].value)
         return list_data
@@ -33,31 +33,42 @@ def processing_file(fn):
         print(f'ERROR... Please try again.')
 
 
-def start_tnclient(fn):
-    data_file = processing_file(fn)
-    tn = telnetlib.Telnet(HOST, PORT, timeout=1)
-    print('Connecting server...' + '\n')
-    for data in data_file:
-        tn.write(data.encode() + b'\n')
-        time.sleep(0.1)
-    print(tn.read_very_eager().decode())
-    tn.close()
-    print('Finished...' + '\n' + 'Goodbye!')
+def start_tnclient(choose_input):
+    if choose_input == 1:
+        file_name = input('Enter path you file: ').strip()
+        data_file = processing_file(file_name)
+        tn = telnetlib.Telnet(HOST, PORT, timeout=1)
+        print('Connecting server...' + '\n')
+        for data in data_file:
+            tn.write(data.encode() + b'\n')
+            time.sleep(0.1)
+        print(tn.read_very_eager().decode())
+        tn.close()
+        print('Finished...')
+    elif choose_input == 2:
+        tn = telnetlib.Telnet(HOST, PORT, timeout=1)
+        print('Connecting server...' + '\n')
+        while True:
+            data_input = input('Enter data or enter Q for exit: ')
+            if data_input == 'Q':
+                break
+            tn.write(data_input.encode())
+            time.sleep(0.1)
+            print(tn.read_very_eager().decode())
+        tn.close()
 
 
 def main():
     while True:
         try:
-            file_name = input('Enter path you file: ').strip()
-            start_tnclient(file_name)
+            choose_input = int(input('Choose, File(1) or Input(2): '))
+            start_tnclient(choose_input)
         except ConnectionRefusedError:
             print('Connection error... Please try again or press CTRL+C for exit.')
         except FileNotFoundError:
             print('File not found... Please try again or press CTRL+C for exit.')
         except KeyboardInterrupt:
-            print('\n'+'Goodbye!')
-            break
-        else:
+            print('\n' + 'Goodbye!')
             break
 
 
